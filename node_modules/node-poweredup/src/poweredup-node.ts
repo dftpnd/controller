8a06ba1677332fa526e5647bb199e1b60bfe959e
Maksim Gryaznov <mgryaznov@alfabank.ru>
2020-02-20 10:48:08 +0300

    init

---
+++ node_modules/node-poweredup/src/poweredup-node.ts
@@ -0,0 +1,200 @@
+import { Peripheral } from "@abandonware/noble";
+
+import { NobleDevice } from "./nobleabstraction";
+
+import { BaseHub } from "./hubs/basehub";
+import { DuploTrainBase } from "./hubs/duplotrainbase";
+import { Hub } from "./hubs/hub";
+import { MoveHub } from "./hubs/movehub";
+import { RemoteControl } from "./hubs/remotecontrol";
+import { TechnicMediumHub } from "./hubs/technicmediumhub";
+import { WeDo2SmartHub } from "./hubs/wedo2smarthub";
+
+import * as Consts from "./consts";
+
+import { EventEmitter } from "events";
+
+import Debug = require("debug");
+const debug = Debug("poweredup");
+import noble = require("@abandonware/noble");
+
+let ready = false;
+let wantScan = false;
+let discoveryEventAttached = false;
+
+const startScanning = () => {
+    noble.startScanning();
+};
+
+noble.on("stateChange", (state: string) => {
+    ready = (state === "poweredOn");
+    if (ready) {
+        if (wantScan) {
+            debug("Scanning started");
+            startScanning();
+        }
+    } else {
+        noble.stopScanning();
+    }
+});
+
+/**
+ * @class PoweredUP
+ * @extends EventEmitter
+ */
+export class PoweredUP extends EventEmitter {
+
+
+    private _connectedHubs: {[uuid: string]: BaseHub} = {};
+
+
+    constructor () {
+        super();
+        this._discoveryEventHandler = this._discoveryEventHandler.bind(this);
+    }
+
+
+    /**
+     * Begin scanning for Powered UP Hub devices.
+     * @method PoweredUP#scan
+     */
+    public async scan () {
+        wantScan = true;
+
+        if (!discoveryEventAttached) {
+            noble.on("discover", this._discoveryEventHandler);
+            discoveryEventAttached = true;
+        }
+
+        if (ready) {
+            debug("Scanning started");
+            startScanning();
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Stop scanning for Powered UP Hub devices.
+     * @method PoweredUP#stop
+     */
+    public stop () {
+        wantScan = false;
+
+        if (discoveryEventAttached) {
+            noble.removeListener("discover", this._discoveryEventHandler);
+            discoveryEventAttached = false;
+        }
+
+        noble.stopScanning();
+    }
+
+
+    /**
+     * Retrieve a list of Powered UP Hubs.
+     * @method PoweredUP#getHubs
+     * @returns {BaseHub[]}
+     */
+    public getHubs () {
+        return Object.values(this._connectedHubs);
+    }
+
+
+    /**
+     * Retrieve a Powered UP Hub by UUID.
+     * @method PoweredUP#getHubByUUID
+     * @param {string} uuid
+     * @returns {BaseHub | null}
+     */
+    public getHubByUUID (uuid: string) {
+        return this._connectedHubs[uuid];
+    }
+
+
+    /**
+     * Retrieve a Powered UP Hub by primary MAC address.
+     * @method PoweredUP#getHubByPrimaryMACAddress
+     * @param {string} address
+     * @returns {BaseHub}
+     */
+    public getHubByPrimaryMACAddress (address: string) {
+        return Object.values(this._connectedHubs).filter((hub) => hub.primaryMACAddress === address)[0];
+    }
+
+
+    /**
+     * Retrieve a list of Powered UP Hub by name.
+     * @method PoweredUP#getHubsByName
+     * @param {string} name
+     * @returns {BaseHub[]}
+     */
+    public getHubsByName (name: string) {
+        return Object.values(this._connectedHubs).filter((hub) => hub.name === name);
+    }
+
+
+    /**
+     * Retrieve a list of Powered UP Hub by type.
+     * @method PoweredUP#getHubsByType
+     * @param {string} name
+     * @returns {BaseHub[]}
+     */
+    public getHubsByType (hubType: number) {
+        return Object.values(this._connectedHubs).filter((hub) => hub.type === hubType);
+    }
+
+
+    private async _discoveryEventHandler (peripheral: Peripheral) {
+
+        peripheral.removeAllListeners();
+        const device = new NobleDevice(peripheral);
+
+        let hub: BaseHub;
+
+        if (await WeDo2SmartHub.IsWeDo2SmartHub(peripheral)) {
+            hub = new WeDo2SmartHub(device);
+        } else if (await MoveHub.IsMoveHub(peripheral)) {
+            hub = new MoveHub(device);
+        } else if (await Hub.IsHub(peripheral)) {
+            hub = new Hub(device);
+        } else if (await RemoteControl.IsRemoteControl(peripheral)) {
+            hub = new RemoteControl(device);
+        } else if (await DuploTrainBase.IsDuploTrainBase(peripheral)) {
+            hub = new DuploTrainBase(device);
+        } else if (await TechnicMediumHub.IsTechnicMediumHub(peripheral)) {
+            hub = new TechnicMediumHub(device);
+        } else {
+            return;
+        }
+
+        device.on("discoverComplete", () => {
+
+            hub.on("connect", () => {
+                debug(`Hub ${hub.uuid} connected`);
+                this._connectedHubs[hub.uuid] = hub;
+            });
+
+            hub.on("disconnect", () => {
+                debug(`Hub ${hub.uuid} disconnected`);
+                delete this._connectedHubs[hub.uuid];
+
+                if (wantScan) {
+                    startScanning();
+                }
+            });
+
+            debug(`Hub ${hub.uuid} discovered`);
+
+            /**
+             * Emits when a Powered UP Hub device is found.
+             * @event PoweredUP#discover
+             * @param {WeDo2SmartHub | MoveHub | TechnicMediumHub | RemoteControl | DuploTrainBase} hub
+             */
+            this.emit("discover", hub);
+
+        });
+
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,107 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const PoweredUP = require("node-poweredup");
+const poweredUP = new PoweredUP.PoweredUP();
+
+function MakeQuerablePromise(promise) {
+  debugger;
+  // Don't create a wrapper for promises that can already be queried.
+  if (promise.isResolved) return promise;
+
+  var isResolved = false;
+  var isRejected = false;
+
+  // Observe the promise, saving the fulfillment in a closure scope.
+  var result = promise.then(
+     function(v) { isResolved = true; return v; }, 
+     function(e) { isRejected = true; throw e; });
+  result.isFulfilled = function() { return isResolved || isRejected; };
+  result.isResolved = function() { return isResolved; }
+  result.isRejected = function() { return isRejected; }
+  return result;
+}
+
+poweredUP.on("discover", async (hub) => { 
+  console.log(`Discovered ${hub.name}!`);
+  // hub.batteryLevel
+  console.log('batteryLevel:', hub.batteryLevel)
+  await hub.connect(); 
+  const motorA = await hub.waitForDeviceAtPort("A"); 
+  const motorB = await hub.waitForDeviceAtPort("B");
+  const motorC = await hub.waitForDeviceAtPort("C");
+
+
+  // const lightD = await hub.waitForDeviceAtPort("D");
+  
+  console.log("Lego hub connected");
+
+
+  // lightD.setBrightness(100);
+  // motorC.gotoAngle(120);
+
+  let rotationAction;
+
+
+  io.on('connection', function(socket){
+    console.log('io on connection')
+    const state = {
+      M1: false,
+      M2: false,
+    };
+
+    socket.on('action', function(args) {
+      const { M1, M2 } = args;
+      console.log('M1', M1)
+      const ANGLE = Math.round((args.AXES[0] + 1) * 89);
+
+      debugger;
+
+      if (!rotationAction || rotationAction.isResolved()) {
+        console.log('ANGLE', ANGLE);
+        rotationAction = MakeQuerablePromise(
+          motorC.gotoAngle(ANGLE, 10)
+          );
+        debugger;
+      }
+
+
+
+      
+
+      if (M1 !== 0 && !state.M1) {
+        state.M1 = M1;
+        motorA.setPower(100);
+        motorB.setPower(100);
+      } else if (M1 === 0 && state.M1) {
+        state.M1 = false;
+        motorA.brake();
+        motorB.brake();
+      }
+      
+      if (M2 !== 0 && !state.M2) {
+        state.M2 = M2;
+        motorA.setPower(-100);
+        motorB.setPower(-100);
+      } else if (M2 === 0 && state.M2) {
+        state.M2 = false;
+        motorA.brake();
+        motorB.brake();
+      }
+    });
+  });
+});
+
+
+app.get('/', function(_, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+poweredUP.scan();
