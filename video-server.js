b63e4f50aadaaea3a92eb2c765b40cc428489267
Maksim Gryaznov <mgryaznov@alfabank.ru>
2020-03-06 11:56:31 +0300

    1

--- video-server.js
+++ video-server.js
@@ -47,10 +47,16 @@ app.get('/', function (req, res) {
                 '!', 'tcpclientsink', 'host=localhost',
                 'port=' + tcpServer.address().port];
 
+
+                //gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM),width=3820, height=2464, framerate=21/1, format=NV12' ! nvvidconv flip-method=0 ! 'video/x-raw,width=960, height=616' ! nvvidconv ! nvegltransform ! 
+
                 var args2 =
                 ['nvarguscamerasrc',
                     '!', 'video/x-raw(memory:NVMM),width=3820, height=2464, framerate=21/1',
-                    '!', 'm.', 'webmmux', 'name=m', 'streamable=true',
+                    '!', 'nvvidconv flip-method=0',
+                    '!', 'video/x-raw,width=960, height=616',
+                    '!', 'nvvidconv',
+                    '!', 'nvegltransform',
                     '!', 'tcpclientsink', 'host=localhost',
                     'port=' + tcpServer.address().port];
 
